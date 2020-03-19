8ba11c4e9db343cb2cf33e7b53cae53b2b6f4288
andressaggudelo <andresagudelo9239@gmail.com>
2020-03-18 21:54:45 -0500

    Function converted to Widget, more efficient code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './quote.dart';
+import './quote_card.dart';
 
 void main() => runApp(MaterialApp(
   home: QuoteList(),
@@ -21,35 +22,6 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'Linus Naughty', text: 'Software is like sex: it\'s better when it\'s free'),
   ];
 
-  Widget quoteTemplate(quote){
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text,   // Also could: '${quote.text}',
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(height: 6.0,),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,7 +37,7 @@ class _QuoteListState extends State<QuoteList> {
         padding: EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+          children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
         ),
       ),
     );
