cd4063b756c7d450bbbf418f88f7048e9447d102
andressaggudelo <andresagudelo9239@gmail.com>
2020-03-18 21:01:58 -0500

    Function that returns a Card Widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,39 @@ class _QuoteListState extends State<QuoteList> {
 
   List<Quote> quotes = [
     Quote(author: 'Linus Torvalds', text: 'In my opinion MS is a lot better at making money than it is at making good operating systems'),
-    Quote(author: 'Linus v2', text: 'A computer is like air conditioning, it becomes useless when you open Windows'),
-    Quote(text: 'Talk is cheap, show me the code', author: 'linus Direct'),
+    Quote(author: 'Linus LOL', text: 'A computer is like air conditioning, it becomes useless when you open Windows'),
+    Quote(text: 'Talk is cheap, show me the code', author: 'Linus Direct'),
     Quote(author: 'Linus Naughty', text: 'Software is like sex: it\'s better when it\'s free'),
   ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,   // Also could: '${quote.text}',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6.0,),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,14 +65,7 @@ class _QuoteListState extends State<QuoteList> {
         padding: EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: quotes.map((quote) => Text(
-            '${quote.text} - ${quote.author}',
-            style: TextStyle(
-              fontSize: 18.0,
-              letterSpacing: 1.0,
-            ),
-          ),
-          ).toList(),
+          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
         ),
       ),
     );
